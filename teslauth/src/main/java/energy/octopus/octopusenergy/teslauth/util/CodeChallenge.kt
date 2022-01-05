46fff1e9a1fbd8400e53a55b1953bdf3442923a2
nrobi144 <robert.nagy.dev@gmail.com>
2022-01-05 12:38:56 +0200

    Make challenge url safe

--- teslauth/src/main/java/energy/octopus/octopusenergy/teslauth/util/CodeChallenge.kt
+++ teslauth/src/main/java/energy/octopus/octopusenergy/teslauth/util/CodeChallenge.kt
@@ -2,10 +2,11 @@ package energy.octopus.octopusenergy.teslauth.util
 
 import android.util.Base64
 import android.util.Base64.NO_PADDING
+import android.util.Base64.URL_SAFE
 import org.apache.commons.lang3.RandomStringUtils
 
 internal class CodeChallenge {
     val verifier = RandomStringUtils.randomAlphabetic(86)
-    val challenge = Base64.encodeToString(verifier.toByteArray(), NO_PADDING)
+    val challenge = Base64.encodeToString(verifier.toByteArray(), NO_PADDING or URL_SAFE)
     val method = "S256"
 }
