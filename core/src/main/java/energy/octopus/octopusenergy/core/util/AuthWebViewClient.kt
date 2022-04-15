163c2777031a11d1810e7e0b7f2b283a0f63744c
Róbert Nagy <robert.nagy.dev@gmail.com>
2022-04-15 12:59:46 +0300

    Make sure url comparison is correct

--- core/src/main/java/energy/octopus/octopusenergy/core/util/AuthWebViewClient.kt
+++ core/src/main/java/energy/octopus/octopusenergy/core/util/AuthWebViewClient.kt
@@ -4,6 +4,8 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import energy.octopus.octopusenergy.core.logging.Logger
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 internal class AuthWebViewClient(
     private val onCodeParsed: (String?) -> Unit,
@@ -21,10 +23,15 @@ internal class AuthWebViewClient(
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?) =
         request?.url?.toString()?.let {
             Logger.log(it)
-            if (it.startsWith(redirectUri)) {
+            if (it.containsRedirectUri) {
                 code = it.substringAfter("$resultParameterKey=").substringBefore("&")
                 return@let true
             }
             false
         } ?: false
+
+    private val String.containsRedirectUri
+        get() = decode(url = this).startsWith(decode(url = redirectUri))
+
+    private fun decode(url: String) = URLDecoder.decode(url, StandardCharsets.UTF_8.toString())
 }
