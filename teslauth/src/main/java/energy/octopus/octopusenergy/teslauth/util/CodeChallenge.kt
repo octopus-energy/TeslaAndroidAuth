cc4edbbd1cd7b6d99d480ec2af749f33abc471b7
Paul Davies <pauldavies83@users.noreply.github.com>
2023-02-17 15:47:59 +0000

    challenge should be an sha256 of the verifier

--- teslauth/src/main/java/energy/octopus/octopusenergy/teslauth/util/CodeChallenge.kt
+++ teslauth/src/main/java/energy/octopus/octopusenergy/teslauth/util/CodeChallenge.kt
@@ -2,11 +2,12 @@ package energy.octopus.octopusenergy.teslauth.util
 
 import android.util.Base64
 import android.util.Base64.NO_PADDING
+import android.util.Base64.NO_WRAP
 import android.util.Base64.URL_SAFE
 import org.apache.commons.lang3.RandomStringUtils
 
 internal class CodeChallenge {
-    val verifier = RandomStringUtils.randomAlphabetic(86)
-    val challenge = Base64.encodeToString(verifier.toByteArray(), NO_PADDING or URL_SAFE)
+    val verifier: String = RandomStringUtils.randomAlphabetic(86)
+    val challenge: String = Base64.encodeToString(verifier.sha256(), NO_PADDING or NO_WRAP or URL_SAFE)
     val method = "S256"
 }
